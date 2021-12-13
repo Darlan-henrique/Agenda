0e8c36bdad0b0f401018f847c24aa8927a681050
henrique darlan <henrique.darlan@hotmail.com>
2021-12-13 15:03:37 -0300

    Add project files.

---
+++ Agenda/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace Agenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.txtCelular = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.bntDeletar = new System.Windows.Forms.Button();
+            this.dvgAgenda = new System.Windows.Forms.DataGridView();
+            this.btnAbout = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgAgenda)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(192, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AGENDA - CONTATOS";
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnNovo.Location = new System.Drawing.Point(767, 27);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(109, 55);
+            this.btnNovo.TabIndex = 1;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = false;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Celular";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(28, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 38);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Telefone";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(28, 237);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 25);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 38);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 38);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Endereço";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(183, 80);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(294, 26);
+            this.txtNome.TabIndex = 9;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(183, 119);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(294, 26);
+            this.txtEndereco.TabIndex = 10;
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.Location = new System.Drawing.Point(183, 159);
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(192, 26);
+            this.txtCelular.TabIndex = 11;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(183, 198);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(192, 26);
+            this.txtTelefone.TabIndex = 12;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(183, 237);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(304, 26);
+            this.txtEmail.TabIndex = 13;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnSalvar.Location = new System.Drawing.Point(767, 94);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(109, 55);
+            this.btnSalvar.TabIndex = 14;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.BackColor = System.Drawing.Color.Yellow;
+            this.btnAtualizar.Location = new System.Drawing.Point(767, 170);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(109, 55);
+            this.btnAtualizar.TabIndex = 15;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = false;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnSair.Location = new System.Drawing.Point(767, 389);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(109, 55);
+            this.btnSair.TabIndex = 16;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // bntDeletar
+            // 
+            this.bntDeletar.BackColor = System.Drawing.Color.Crimson;
+            this.bntDeletar.Location = new System.Drawing.Point(767, 241);
+            this.bntDeletar.Name = "bntDeletar";
+            this.bntDeletar.Size = new System.Drawing.Size(109, 55);
+            this.bntDeletar.TabIndex = 17;
+            this.bntDeletar.Text = "Deletar";
+            this.bntDeletar.UseVisualStyleBackColor = false;
+            this.bntDeletar.Click += new System.EventHandler(this.bntDeletar_Click);
+            // 
+            // dvgAgenda
+            // 
+            this.dvgAgenda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgAgenda.Location = new System.Drawing.Point(31, 287);
+            this.dvgAgenda.Name = "dvgAgenda";
+            this.dvgAgenda.RowHeadersWidth = 62;
+            this.dvgAgenda.RowTemplate.Height = 28;
+            this.dvgAgenda.Size = new System.Drawing.Size(690, 213);
+            this.dvgAgenda.TabIndex = 18;
+            this.dvgAgenda.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dvgAgenda_CellContentClick);
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(767, 461);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(109, 35);
+            this.btnAbout.TabIndex = 19;
+            this.btnAbout.Text = "Sobre";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(903, 508);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.dvgAgenda);
+            this.Controls.Add(this.bntDeletar);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtCelular);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "AGENDA";
+            ((System.ComponentModel.ISupportInitialize)(this.dvgAgenda)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.TextBox txtCelular;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button bntDeletar;
+        private System.Windows.Forms.DataGridView dvgAgenda;
+        private System.Windows.Forms.Button btnAbout;
+    }
+}
+
---
+++ Agenda/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Agenda
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conexao = new SqlConnection(
+            "Data Source=.;Initial " +
+            "Catalog=Agenda;Integrated Security=True");
+        SqlCommand command;
+        SqlDataAdapter adapter;
+        int ID = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ExibirDados();
+        }
+
+        private void ExibirDados()
+        {
+            try
+            {
+                conexao.Open();
+                DataTable dt = new DataTable();
+                adapter = new SqlDataAdapter("SELECT * FROM Contatos", conexao);
+                adapter.Fill(dt);
+                dvgAgenda.DataSource = dt;
+
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        private void LimparDados()
+        {
+            txtNome.Text = "";
+            txtEndereco.Text = "";
+            txtCelular.Text = "";
+            txtTelefone.Text = "";
+            txtEmail.Text = "";
+            ID = 0;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja Sair do programa ?", "Agenda", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+            else
+            {
+                txtNome.Focus();
+            }
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = "";
+            txtEndereco.Text = "";
+            txtCelular.Text = "";
+            txtTelefone.Text = "";
+            txtEmail.Text = "";
+            txtNome.Focus();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text != "" 
+                && txtEndereco.Text != "" 
+                && txtCelular.Text != "" 
+                && txtTelefone.Text != ""
+                && txtEmail.Text != "")
+            {
+                try
+                {
+                    command = new SqlCommand(" INSERT INTO Contatos (nome,endereco,celular,telefone,email) VALUES(@nome,@endereco,@celular,@telefone,@email)", conexao);
+                    conexao.Open();
+                    command.Parameters.AddWithValue("@nome",txtNome.Text.ToUpper());
+                    command.Parameters.AddWithValue("@endereco", txtEndereco.Text.ToUpper());
+                    command.Parameters.AddWithValue("@celular", txtCelular.Text.ToUpper());
+                    command.Parameters.AddWithValue("@telefone", txtTelefone.Text.ToUpper());
+                    command.Parameters.AddWithValue("@email", txtEmail.Text.ToLower());
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Registro incluído com sucesso...");
+
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Erro: " + ex.Message);
+                }
+                finally
+                {
+                    conexao.Close();
+                    ExibirDados();
+                    LimparDados();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Informe todos os dados requeridos");
+            }
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text != "" 
+                && txtEndereco.Text != ""
+                && txtCelular.Text != ""
+                && txtTelefone.Text != ""
+                && txtEmail.Text != "")
+            {
+                try
+                {
+                    command = new SqlCommand("UPDATE Contatos SET nome =@nome, endereco =@endereco,celular =@celular,telefone =@telefone,email =@email WHERE id=@id", conexao);
+                    conexao.Open();
+                    command.Parameters.AddWithValue("@id", ID);
+                    command.Parameters.AddWithValue("@nome", txtNome.Text.ToUpper());
+                    command.Parameters.AddWithValue("@endereco", txtEndereco.Text.ToUpper());
+                    command.Parameters.AddWithValue("@celular", txtCelular.Text.ToUpper());
+                    command.Parameters.AddWithValue("@telefone", txtTelefone.Text.ToUpper());
+                    command.Parameters.AddWithValue("@email", txtEmail.Text.ToLower());
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Registro atualizado com sucesso...");
+
+                }
+
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Erro: " + ex.Message);
+                }
+                finally
+                {
+                    conexao.Close();
+                    ExibirDados();
+                    LimparDados();
+                }
+
+            }
+
+            else
+            {
+                MessageBox.Show("Informe todos os dados requeridos");
+            }
+        }
+
+        private void bntDeletar_Click(object sender, EventArgs e)
+        {
+            if (ID != 0)
+            {
+                if(MessageBox.Show("Deseja Deletar este registro ?", "Agenda", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        command = new SqlCommand("DELETE Contatos WHERE id=@id", conexao);
+                        conexao.Open();
+                        command.Parameters.AddWithValue("@id", ID);
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Registro deletado com sucesso...!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Erro : " + ex.Message);
+                    }
+                    finally
+                    {
+                        conexao.Close();
+                        ExibirDados();
+                        LimparDados();
+  
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selecione um registro para deletar");
+            }
+        }
+
+        private void dvgAgenda_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           try
+            {
+                ID = Convert.ToInt32(dvgAgenda.Rows[e.RowIndex].Cells[0].Value.ToString());
+                txtNome.Text = dvgAgenda.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtEndereco.Text = dvgAgenda.Rows[e.RowIndex].Cells[2].Value.ToString();
+                txtCelular.Text = dvgAgenda.Rows[e.RowIndex].Cells[3].Value.ToString();
+                txtTelefone.Text = dvgAgenda.Rows[e.RowIndex].Cells[4].Value.ToString();
+                txtEmail.Text = dvgAgenda.Rows[e.RowIndex].Cells[5].Value.ToString();
+            }
+            catch { }
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Adptado por Henrique Darlan .NET", "Agenda", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            txtNome.Focus();
+        }
+    }
+}
